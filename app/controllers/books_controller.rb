c5943204353b1970bef2fb0fd1f117f4cf2d8f46
chawannn <158204549+chawannn@users.noreply.github.com>
2024-02-23 13:33:10 +0000

    index途中 2024/02/23

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,28 @@
 class BooksController < ApplicationController
+
+  def index
+    @books =Book.all
+  end
+
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
