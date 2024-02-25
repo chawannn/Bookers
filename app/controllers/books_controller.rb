c1e76e1f5fabaf1541e9033305fed635c9eb79cf
chawannn <158204549+chawannn@users.noreply.github.com>
2024-02-25 07:09:41 +0000

    lay out 2024/02/25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
@@ -25,7 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
